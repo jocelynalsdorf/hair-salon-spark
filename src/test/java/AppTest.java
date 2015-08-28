563e67ad6b7385f6b0f77e60f411a1e94159196d
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-28 10:31:58 -0700

    passing routes for deleting a specific client from a stylist

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -60,6 +60,18 @@ public class AppTest extends FluentTest{
 	}
 
 	@Test
+	public void clientIsDisplayedWhenCreated() {
+		Stylist myStylist = new Stylist("joan");
+		myStylist.save();
+		goTo("http://localhost:4567/");
+		fill("#description").with("sam");
+		submit(".btn-client");
+		String stylistPath = String.format("http://localhost:4567/stylists/%d", myStylist.getId());
+		goTo(stylistPath);
+		assertThat(pageSource()).contains("sam");
+	}
+
+	@Test
 	public void allclientsDisplayDescriptionOnStylistPage() {
 		Stylist myStylist = new Stylist("Leon");
 		myStylist.save();
@@ -71,6 +83,22 @@ public class AppTest extends FluentTest{
 		goTo(stylistPath);
 		assertThat(pageSource()).contains("fred");
 		assertThat(pageSource()).contains("greg");
+	}
+
+	@Test
+	public void clientIsDeleted() {
+		Stylist myStylist = new Stylist("jim");
+		myStylist.save();
+		Client firstClient = new Client("tim", myStylist.getId());
+		Client secondClient = new Client("tom", myStylist.getId()); 
+		firstClient.save();
+		secondClient.save();
+		String stylistPath = String.format("http://localhost:4567/stylists/%d", myStylist.getId());
+		goTo(stylistPath);
+		submit(".tim");
+		click("a", withText("jim"));
+		assertThat(pageSource()).doesNotContain("tim");
+		assertThat(pageSource()).contains("tom");
 	}
 
 
