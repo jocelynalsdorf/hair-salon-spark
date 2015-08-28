f95f9f1e1e90e6c0c1e1dbc272b4988fce94bc67
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-28 09:29:49 -0700

    route for updating and displaying new name for stylists working

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -42,6 +42,23 @@ public class App {
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
+  get("stylists/:id/update", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+    model.put("stylist", stylist);
+    model.put("template", "templates/update-stylist-form.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  post("stylists/:id/update", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+    String name = request.queryParams("name");
+    stylist.update(name);
+    response.redirect("/");
+    return null;
+  });
+
 
   }//end of main
 }//end of app class
