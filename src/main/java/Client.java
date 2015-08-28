3f1fd2e8d0524d7045f2d2e708c154d60f33f2b7
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-28 08:25:06 -0700

    passing unit test for clients all method

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,4 +2,34 @@ import java.util.List;
 import org.sql2o.*;
 
 
-public class Client {}
+public class Client {
+
+	private int id;
+  private String description;
+  private int stylist_Id;
+
+  public int getId() {
+    return id;
+  }
+
+  public int getStylistId() {
+    return stylist_Id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Client(String description, int stylist_Id) {
+    this.description = description;
+    this.stylist_Id = stylist_Id;
+  }
+
+ public static List<Client> all() {
+  String sql = "SELECT id, description, stylist_Id FROM clients";
+  try(Connection con = DB.sql2o.open()) {
+    return con.createQuery(sql).executeAndFetch(Client.class);
+  }
+ }
+ 
+}//end of client class
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -4,10 +4,14 @@ import org.junit.Rule;
 
 public class ClientTest {
 
-     @Rule
-     public DatabaseRule database = new DatabaseRule();
+	@Rule
+	public DatabaseRule database = new DatabaseRule();
+
+	@Test
+	public void all_emptyAtFirst() {
+	 assertEquals(Client.all().size(), 0);
+	}
 
 
 
-
- }
+ }//end of test class
